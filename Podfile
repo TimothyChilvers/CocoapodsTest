50be013a2f6c0926e1ae677b8dc6827d11574b1b
Tim Chilvers <timothy.chilvers@touchsurgery.com>
2018-05-22 14:16:29 +0100

    Add cocoapods

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'PodsTest' do
+    pod 'Alamofire', '4.7.2'
+end
--- PodsTest/AppDelegate.swift
+++ PodsTest/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        _ = Alamofire.SessionManager.default
         return true
     }
 
